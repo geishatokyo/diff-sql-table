efb474fa8bfa012add44414f08ef132929c2c94b
ponkotuy <ponkotuy@geishatokyo.com>
2014-12-11 17:16:39 +0900

    Ver0.3.5

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object DiffSqlBuild extends Build {
 
   val defaultSettings = Project.defaultSettings ++ Seq(
     organization := "com.geishatokyo.tools",
-    version := "0.3.4-SNAPSHOT",
+    version := "0.3.5-SNAPSHOT",
     scalaVersion := "2.11.2",
     crossScalaVersions := Seq("2.11.2","2.10.4"),
     scalacOptions <++= (scalaVersion) map { v =>
