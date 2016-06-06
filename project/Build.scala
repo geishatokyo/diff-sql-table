6fdde0f50dca836c716f13e146bbdf9df6454cf7
Takeshita Yoshiteru <takezoux2@gmail.com>
2016-06-07 01:51:39 +0900

    version 0.3.5へ maven centralへのデプロイ準備

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ object DiffSqlBuild extends Build {
 
   val defaultSettings = Project.defaultSettings ++ Seq(
     organization := "com.geishatokyo.tools",
-    version := "0.3.5-SNAPSHOT",
+    version := "0.3.5",
     scalaVersion := "2.11.7",
     crossScalaVersions := Seq("2.11.7","2.10.4"),
     scalacOptions <++= (scalaVersion) map { v =>
@@ -24,10 +24,36 @@ object DiffSqlBuild extends Build {
     }
   )
 
+  def publishToMaven = Command.command("publishToMaven")( state => {
+
+    val switched = Command.process("project diff-sql-table-parser",state)
+
+    val ext = Project.extract(switched)
+    val nexus = "https://oss.sonatype.org/"
+    val snapShot_? = ext.get(isSnapshot)
+
+    // 芸者東京では、global.sbtなどに社内レポジトリを登録しており、その設定と干渉するため、
+    // このように後からデプロイ設定を更新しています。
+    val resolver = if(snapShot_?){
+      Some("snapshots"  at nexus + "content/repositories/snapshots")
+    }else{
+      Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    }
+    val publishState = ext.append(Seq(
+      publishTo := resolver
+    ),switched)
+
+    val lastState = Command.process("publishSigned",publishState)
+
+    state
+  })
+
   lazy val root = Project(
     id = "diff-sql-table",
     base = file("."),
-    settings = defaultSettings
+    settings = defaultSettings ++ Seq(
+      commands += publishToMaven
+    )
   ) aggregate(parser)
 
   lazy val parser = Project(
@@ -49,16 +75,40 @@ object DiffSqlBuild extends Build {
       }),
       publishMavenStyle := true,
       pomIncludeRepository := { _ => false },
-      pomExtra := loadPomExtra()
+      pomExtra := pom,
+      publishTo := {
+        val nexus = "https://oss.sonatype.org/"
+        if (isSnapshot.value)
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+      }
     )
   )
 
-  /**
-   *
-   * @return
-   */
-  def loadPomExtra() = {
-    XML.loadFile( file( "./project/pomExtra.xml")).child
+
+  def pom = {
+    <url>https://github.com/geishatokyo/diff-sql-table</url>
+    <licenses>
+      <license>
+        <name>MIT License</name>
+        <url>http://www.opensource.org/licenses/mit-license.php</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+    <scm>
+      <connection>scm:git:git@github.com:geishatokyo/diff-sql-table.git</connection>
+      <developerConnection>scm:git:git@github.com:geishatokyo/diff-sql-table.git</developerConnection>
+      <url>scm:git:git@github.com:geishatokyo/diff-sql-table.git</url>
+    </scm>
+
+    <developers>
+      <developer>
+        <id>takeshita</id>
+        <name>Yositeru Takeshita</name>
+        <email>takezoux2@gmail.com</email>
+      </developer>
+    </developers>
   }
 
   lazy val scalatest = "org.scalatest" %% "scalatest" % "2.2.1" % "test"
